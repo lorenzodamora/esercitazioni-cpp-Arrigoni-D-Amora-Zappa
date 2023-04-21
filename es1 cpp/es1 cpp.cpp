9924862da26903dc7e6734f26f234275d74a1d8a
lorenzodamora <damora.lorenzo.studente@itispaleocapa.it>
2023-04-21 13:07:33 +0200

    .

--- es1 cpp/es1 cpp.cpp
+++ es1 cpp/es1 cpp.cpp
@@ -108,18 +108,72 @@ int main()
 		switch (ric)
 		{
 		case 1:
-			consumi[1] = 100; //usa 100 grammi di ingrediente 1
-			consumi[2] = 200;
+			consumi[1] = 10; //Aceto di vino bianco ml 500
+			consumi[5] = 100; //Arancia candita g 100
+			consumi[9] = 5; //Cacao amaro in polvere g 75
+			consumi[13] = 60; //Ciliegie candite n 100
+			consumi[20] = 260; //Farina g 1000
+			consumi[23] = 80; //Gocce di cioccolato fondente g 250
+			consumi[32] = 60; //Marsala ml 100
+			consumi[40] = 1; //Olio di semi l 1
+			consumi[49] = 1000; //Ricotta di pecora g 2000
+			consumi[51] = 5; //Sale g 100
+			consumi[55] = 30; //Strutto g 200
+			consumi[56] = 2; //Uova n 12
+			consumi[59] = 150; //Zucchero g 1000
+			consumi[61] = 240; //Zucchero a velo g 1000
 			break;
 
 		case 2:
-			consumi[1] = 300;
-			consumi[2] = 0;
-			consumi[3] = 400;
+			consumi[3] = 150; //Acqua ml 1000
+			consumi[5] = 50; //Arancia candita g 100
+			consumi[16] = 300;// Farina 00 g 1000
+			consumi[22] = 100;//Frutta candita g 150
+			consumi[23] = 100; //Gocce di cioccolato fondente g 250
+			consumi[31]= 15;//Maraschino g 100
+			consumi[33] = 200;//Marzapane g 500
+			consumi[45] = 20;//Pasta di pistacchi g 100
+			consumi[49] = 1200; //Ricotta di pecora g 2000
+			consumi[51] = 1; //Sale g 100
+			consumi[54] = 1;//Scorza di limone n 10
+			consumi[56] = 11; //Uova n 12
+			consumi[59] = 350; //Zucchero g 1000
+			consumi[60] = 150; //Zucchero a velo vanigliato g 1000
+			consumi[61] = 630; //Zucchero a velo g 1000
 			break;
 
+		case 3:
+			consumi[3] = 100; //Acqua ml 1000
+			consumi[6] = 1; //Baccello di vaniglia n 5
+			consumi[15] = 500; //Farina 0 g 1000
+			consumi[27] = 175; //Latte intero ml 1000
+			consumi[28] = 13; //Lievito di birra g 20
+			consumi[36] = 10; //Miele g 100
+			consumi[41] = 75; //Olio d'oliva ml 500
+			consumi[43] = 500; //Panna liquida g 600
+			consumi[47] = 50; //Pinoli g 100
+			consumi[51] = 10; //Sale g 100
+			consumi[53] = 1; //Scorza d'arancia n 10
+			consumi[56] = 3; //Uova n 12
+			consumi[57] = 90; //Uvetta g 150
+			consumi[59] = 55; //Zucchero g 1000
+			consumi[61] = 750; //Zucchero a velo g 1000
+			break;
+		case 4:
+			consumi[4] = 15; //Amarene sciroppate n 40
+			consumi[6] = 2; //Baccello di vaniglia n 5
+			consumi[7] = 250; //Burro g 400
+			consumi[16] = 700; //Farina 00 g 1000
+			consumi[26] = 500; //Latte ml 1000
+			consumi[43] = 45; //Panna liquida g 600
+			consumi[56] = 11; //Uova n 12
+			consumi[61] = 200; //Zucchero a velo g 1000
+			break;
+		case 5:
+			consumi[] = ; //
+			break;
 		default:
-			cout << "Questo codice è solo rappresentativo";
+			//
 			break;
 		}
 
